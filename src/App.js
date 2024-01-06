8aa124894e0d5fee196c1b9b1024db7e2cb8aa4f
mkc-c <abdirisak.arr@gmail.com>
2024-01-06 05:15:51 +0000

    render accordion components from seed data, create state for accordion open/close

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 const faqs = [
@@ -18,11 +19,37 @@ const faqs = [
 export default function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-function Accordion() {
-  return <div>TODO</div>;
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((faq, index) => {
+        return (
+          <AccordionItem
+            key={index}
+            title={faq.title}
+            text={faq.text}
+            number={index}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function AccordionItem({ number, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="item">
+      <p className="number">{number < 9 ? `0${number + 1}` : number + 1}</p>
+      <p className="text">{title}</p>
+      <p className="icon">-</p>
+      <div className="content-box">{text}</div>
+    </div>
+  );
 }
