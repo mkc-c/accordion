83bdd33929972b755cc1e97d80c99d1b9965807a
mkc-c <abdirisak.arr@gmail.com>
2024-01-06 06:07:36 +0000

    fix typo

--- src/App.js
+++ src/App.js
@@ -49,9 +49,9 @@ function AccordionItem({ number, title, text }) {
   }
 
   return (
-    <div className="item" onClick={handleToggle}>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{number < 9 ? `0${number + 1}` : number + 1}</p>
-      <p className="text">{title}</p>
+      <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
       {isOpen && <div className="content-box">{text}</div>}
     </div>
